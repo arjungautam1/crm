e2bda03c50aad413f932076815b7b5c149289376
arjun-sudo <laserarjun@gmail.com>
2020-07-20 21:28:17 +0545

    contact form to server

---
+++ client/src/components/AddContacts.js
@@ -0,0 +1,60 @@
+import React, {Component} from "react";
+
+export default class AddContacts extends Component{
+
+    submitContact(event)
+    {
+        event.preventDefault();
+
+        let contact={
+            firstName:this.refs.firstName.value,
+            lastName:this.refs.lastName.value,
+            email:this.refs.email.value,
+        }
+        fetch("http://localhost:8080/api/contacts",
+            {
+                method :"POST",
+                headers:{
+                    "content-type":"application/json",
+                },
+                body:JSON.stringify(contact),
+
+            })
+            .then(response => response.json());
+        window.location.reload();
+    }
+
+    render() {
+        return (
+            <div className="row">
+                <form className="col s12" onSubmit={this.submitContact.bind(this)}>
+                    <div className="row">
+                        <div className="input-field col s6">
+                            <input placeholder="Enter you first Name" ref="first_name" type="text" className="validate"/>
+                                <label htmlFor="firstName">First Name</label>
+                        </div>.firstName.value,
+            lastName:this.refs.lastName.value;
+                        <div className="input-field col s6">
+                            <input placeholder={"Enter you last Name"} ref="last_name" type="text" className="validate" />
+                                <label htmlFor="lastName">Last Name</label>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="input-field col s12">
+                            <input placeholder={"Enter your email"} ref="email" type="email" className="validate" />
+                                <label htmlFor="email">Email</label>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <button className="waves-effect waves-light btn" type="submit" name="action">Submit</button>
+                    </div>
+
+
+                </form>
+            </div>
+
+    )
+    }
+    }
